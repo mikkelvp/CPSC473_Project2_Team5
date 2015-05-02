93805f8457189209abad7ecc45802d875ab8b712
Somayah <somaya.baflah@gmail.com>
2015-05-02 07:27:40 -0700

    Update app.js

--- local pages/javascripts/app.js
+++ local pages/javascripts/app.js
@@ -1,32 +1,33 @@
- $(function () {
-    // click on current location likn 
+ var map;
+          $(document).ready(function(){
+            map = new GMaps({
+              el: '#map',
+              lat: 33.882322,
+              lng: -117.886511
+            });        
+     // click on current address button
     $("#showMyLocation").click(function (event) {
         event.preventDefault();
         $(this).html('Determining address...');
 
         // to show the red marker 
-        markOutLocation = function (lat, long) {
-             var latlng = new google.maps.LatLng(lat,long);    
-              var options = {     
-              zoom: 15,     
-              center: latlng,     
-              mapTypeId: google.maps.MapTypeId.ROADMAP    
-              };    
-              var map = new google.maps.Map(document.getElementById("map"), options);
+         GMaps.geolocate({
+        success: function(position){
+          var latitude = position.coords.latitude;
+          var longitude = position.coords.longitude;
+          var geocoder = new google.maps.Geocoder();
+          var latLng   = new google.maps.LatLng(latitude,longitude);
 
-              var marker = new google.maps.Marker({      
-                position: latlng,       
-                map: map,       
-                title:"You are here!" // the message once hover over the marker
+          map.setCenter(latitude, longitude);
+          map.addMarker({
+                lat: latitude,
+                lng: longitude,
+                icon:{ // custom marker for current location
+                url: 'http://files.softicons.com/download/toolbar-icons/fatcow-hosting-icons-by-fatcow/png/32/location_pin.png'
+                },
+                title: 'You are here'
               });
-            }
-        // if you give a parmission to your privacy 
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            var geocoder = new google.maps.Geocoder();
-            var latLng   = new google.maps.LatLng(latitude,longitude);
-            geocoder.geocode({
+             geocoder.geocode({
                 'latLng': latLng
             }, function (results, status) {    
             // to show your current address
@@ -34,35 +35,45 @@
                     var address = results[0].address_components[i];
                     if (address.types[0] == "postal_code") {
                         $('#zipcode').html(address.long_name);
-                        $('#location').html(results[0].formatted_address);
                         $('#showMyLocation').hide();
+                        $("#current").val(results[0].formatted_address);
                     }
                 }
             });
-             // call the marker function
-             markOutLocation(latitude, longitude);
-        }, 
-                // No geolocation  
-                // the defult address will be CSUF addres
-                function () {
-
-                 var latlng = new google.maps.LatLng(33.882322,-117.886511);    
-                  var options = {     
-                  zoom: 12,     
-                  center: latlng,     
-                  mapTypeId: google.maps.MapTypeId.ROADMAP    
-                  };    
-                  var map = new google.maps.Map(document.getElementById("map"), options);
-
-                  var marker = new google.maps.Marker({      
-                    position: latlng,       
-                    map: map,       
-                    title:"Please accept geolocation for me to be able to find you. I've put you in CSUF for now.!"
-                  });
-
-
-                  }
-            );
-        return false;
+        },
+        error: function(error){
+          alert('Geolocation failed: '+error.message);
+        },
+        not_supported: function(){
+          alert("Your browser does not support geolocation");
+        },
+      });
     });
+
+// find the source (current) and distenation location on the map
+ $('#search').click(function(e) {
+        e.preventDefault();
+        var address=[];
+
+        address[0]= $('#current').val().trim();
+        address[1]= $('#distenation').val().trim();
+for (var x=0; x<2; x++)
+{
+        GMaps.geocode({
+          address: address[x],
+          callback: function(results, status){
+            if(status=='OK'){
+              var latlng = results[0].geometry.location;
+              map.setCenter(latlng.lat(), latlng.lng());
+              map.addMarker({
+                lat: latlng.lat(),
+                lng: latlng.lng(),
+              });
+            }
+          }
+        });
+    }
+
+      });
+
 });
