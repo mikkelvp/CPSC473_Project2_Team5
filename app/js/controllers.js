1dab9ae477760e8f0037ff9e8a7670fa369ddd10
Brandon Truong <brandontruong0@csu.fullerton.edu>
2015-05-03 23:11:10 -0700

    Migrate Somaya's Google Maps to Angular
    Not fully Angular, but it works with current design

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -43,12 +43,93 @@ rideshareControllers.controller('SplashScreenCtrl', ['$scope', '$location',
 
 rideshareControllers.controller('HomeCtrl', ['$scope',
   function ($scope) {
+    $scope.btnShowMyLocation = "Current Address";
+    $scope.btnSearch = "Search";
     
-  }]);
+    $scope.map;
+    $scope.$on("$viewContentLoaded", function() {
+      $scope.map = new GMaps({
+        el: '#map',
+        lat: 33.882322,
+        lng: -117.886511
+      });
+    });
+    
+    $scope.showMyLocation = function() {
+      $scope.btnShowMyLocation = "Determining Address...";
+      GMaps.geolocate({
+        success: function(position){
+          $scope.$apply(function() {
+            $scope.btnShowMyLocation = "Current Address";
+          });
+          var latitude = position.coords.latitude;
+          var longitude = position.coords.longitude;
+          var geocoder = new google.maps.Geocoder();
+          var latLng = new google.maps.LatLng(latitude,longitude);
 
-rideshareControllers.controller('LoginCtrl', ['$scope',
-  function ($scope) {
+          $scope.map.setCenter(latitude, longitude);
+          $scope.map.addMarker({
+            lat: latitude,
+            lng: longitude,
+            icon:{ // custom marker for current location
+              url: 'https://cdn2.iconfinder.com/data/icons/IconsLandVistaMapMarkersIconsDemo/48/MapMarker_Flag_Azure.png'
+            },
+            animation:google.maps.Animation.DROP,
+            title: 'You are here'
+          });
+          // to show your current address
+          geocoder.geocode({
+            'latLng': latLng
+          },
+          function (results, status) {    
+            for (var i = 0; i < results[0].address_components.length; i++) {
+              var address = results[0].address_components[i];
+              if (address.types[0] == "postal_code") {
+                $('#zipcode').html(address.long_name);
+                //$('#showMyLocation').hide(); // No need to hide the button after finding the current address
+                $("#start").val(results[0].formatted_address);
+              }
+            }
+          });
+        },
+        error: function(error){
+          alert('Geolocation failed: '+error.message);
+        },
+        not_supported: function(){
+          alert("Your browser does not support geolocation");
+        },
+      });
+    }; // showMyLocation
     
+    $scope.search = function() {
+      $scope.btnSearch = "Searching...";
+      var directionsDisplay = new google.maps.DirectionsRenderer();
+      var directionsService = new google.maps.DirectionsService();
+
+      var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(33.882322, -117.886511)
+      };
+      var map = new google.maps.Map($('#map')[0], mapOptions);
+      directionsDisplay.setMap(map);
+      directionsDisplay.setPanel($('#directions-panel')[0]);
+
+      var start = $('#start').val();
+      var end = $('#end').val();
+      var request = {
+        origin:start,
+        destination:end,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        }
+        $scope.$apply(function() {
+          $scope.btnSearch = "Search";
+        });
+      });
+    };
   }]);
 
 rideshareControllers.controller('NewRideCtrl', ['$scope',
