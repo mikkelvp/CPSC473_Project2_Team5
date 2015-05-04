5c4cb55b7ad2e172b5912e62e731199c8952b420
Brandon Truong <brandontruong0@csu.fullerton.edu>
2015-05-03 19:21:06 -0700

    Add g+ login button to angular implementation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,44 @@
 var rideshareControllers = angular.module('rideshareControllers', []);
 
 rideshareControllers.controller('SplashScreenCtrl', ['$scope',
-  function ($scope) {
+  function ($scope) {    
+    $scope.processAuth = function (authResult) {
+      // Do a check if authentication has been successful.
+      if (authResult['access_token']) {
+        // Successful sign in.
+
+        //     ...
+        // Do some work [1].
+        //     ...
+      } else if (authResult['error']) {
+        // Error while signing in.
+
+        // Report error.
+        console.log("Could not log in successfully.");
+      }
+    };
+    
+    $scope.signInCallback = function(authResult) {
+      $scope.$apply(function() {
+        $scope.processAuth(authResult);
+      });
+    };
+    
+    $scope.renderGPlusLoginBtn = function () {
+      gapi.signin.render('signInButton',
+        {
+          "callback": $scope.signInCallback, // Function handling the callback.
+          "clientid": "683077495017-rcddi5bfi76bjdmd1bv154n9iip44o5i.apps.googleusercontent.com",
+          "requestvisibleactions": "http://schema.org/AddAction",
+          "cookiepolicy": "single_host_origin",
+          "scope": "https://www.googleapis.com/auth/plus.login"
+        }
+      );
+    };
+    
+    $scope.$on("$viewContentLoaded", function() {
+      $scope.renderGPlusLoginBtn();
+    });
     
   }]);
 
