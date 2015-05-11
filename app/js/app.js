bb8089b3e5415a876324e2c43545f2f208aecc33
Mikkel Petersen <mikkelvp@gmail.com>
2015-05-10 18:30:33 -0700

    fix typo

--- app/js/app.js
+++ app/js/app.js
@@ -1,57 +1,58 @@
 var rideshareApp = angular.module('rideshareApp', ['ngRoute', 'rideshareControllers']);
 
 rideshareApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-                $routeProvider
-                    .when('/', {
-                        templateUrl: 'views/login.html',
-                        controller: 'loginCtrl'
-                    })
-                    .when('/home', {
-                        templateUrl: 'views/home.html',
-                        controller: 'homeCtrl'
-                    })
-                    .when('/result', {
-                        templateUrl: 'views/searchResults.html',
-                        controller: 'searchResultsCtrl'
-                    })
-                    .when('/newRide', {
-                        templateUrl: 'views/newRide.html',
-                        controller: 'newRideCtrl'
-                    })
-                    .when('/myRides', {
-                        templateUrl: 'views/myRides.html',
-                        controller: 'myRidesCtrl'
-                    })
-                    .when('/ride', {
-                        templateUrl: 'views/rideDetails.html',
-                        controller: 'rideDetailsCtrl'
-                            .when('/chat', {
-                                templateUrl: 'views/rideChat.html',
-                                controller: 'rideChatCtrl'
-                            })
-                            .when('/404', {
-                                templateUrl: 'views/404.html',
-                                controller: ''
-                            })
-                            .otherwise({
-                                redirectTo: '/404'
-                            });
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/login.html',
+            controller: 'loginCtrl'
+        })
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/result', {
+            templateUrl: 'views/searchResults.html',
+            controller: 'searchResultsCtrl'
+        })
+        .when('/newRide', {
+            templateUrl: 'views/newRide.html',
+            controller: 'newRideCtrl'
+        })
+        .when('/myRides', {
+            templateUrl: 'views/myRides.html',
+            controller: 'myRidesCtrl'
+        })
+        .when('/ride', {
+            templateUrl: 'views/rideDetails.html',
+            controller: 'rideDetailsCtrl'
+        })
+        .when('/chat', {
+            templateUrl: 'views/rideChat.html',
+            controller: 'rideChatCtrl'
+        })
+        .when('/404', {
+            templateUrl: 'views/404.html',
+            controller: ''
+        })
+        .otherwise({
+            redirectTo: '/404'
+        });
 
-                        // use the HTML5 History API
-                        $locationProvider.html5Mode(true);
-                    }]);
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+}]);
 
-            // directive from http://stackoverflow.com/a/17472118
-            rideshareApp.directive('ngEnter', function() {
-                return function(scope, element, attrs) {
-                    element.bind("keydown keypress", function(event) {
-                        if (event.which === 13) {
-                            scope.$apply(function() {
-                                scope.$eval(attrs.ngEnter);
-                            });
+// directive from http://stackoverflow.com/a/17472118
+rideshareApp.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if (event.which === 13) {
+                scope.$apply(function() {
+                    scope.$eval(attrs.ngEnter);
+                });
 
-                            event.preventDefault();
-                        }
-                    });
-                };
-            });
+                event.preventDefault();
+            }
+        });
+    };
+});
