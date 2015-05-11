18b5743f3754062a1e968035d89d73998a437ab7
Somayah <somaya.baflah@gmail.com>
2015-05-11 10:54:38 -0700

    add directions in new ride page + calander/time button to home page

--- app/controllers/newRideController.js
+++ app/controllers/newRideController.js
@@ -33,6 +33,16 @@ rideshareControllers.controller('newRideCtrl', ['$scope', '$rootScope', '$http',
             });
         };
 
+        $scope.$on("$viewContentLoaded", function() {
+            var mapCanvas = document.getElementById('map-canvas');
+            var mapOptions = {
+                center: new google.maps.LatLng(33.882322, -117.886511),
+                zoom: 8,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(mapCanvas, mapOptions);
+        });
+
         $scope.getCurrentDestination = function getCurrentAddress() {
             var geocoder = new google.maps.Geocoder();
             var latlng;
@@ -46,6 +56,15 @@ rideshareControllers.controller('newRideCtrl', ['$scope', '$rootScope', '$http',
                         if (results) {
                             $scope.$apply(function() {
                                 $scope.source = results[0].formatted_address;
+                                 map.setCenter(latlng);
+                                var marker = new google.maps.Marker({
+                                    position: latlng,
+                                    map: map,
+                                    animation: google.maps.Animation.DROP,
+                                    title: 'You are here'
+                                });
+                                marker.setMap(map);
+                                map.setZoom(12);
                             });
                         } else {
                             alert('No results found');
@@ -56,6 +75,38 @@ rideshareControllers.controller('newRideCtrl', ['$scope', '$rootScope', '$http',
                 });
             });
         };
+
+        $scope.showDirection = function() {
+        if ($scope.destination == null){
+             alert('Please, enter the distnation');
+        } else if ($scope.source == null){
+             alert('Please, enter the source');
+        } else {
+          var directionsDisplay = new google.maps.DirectionsRenderer();
+          var directionsService = new google.maps.DirectionsService();
+
+          var mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(33.882322, -117.886511)
+          };
+          var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+          directionsDisplay.setMap(map);
+
+          var start = $scope.source ;
+          var end = $scope.destination ;
+          var request = {
+            origin:start,
+            destination:end,
+            travelMode: google.maps.TravelMode.DRIVING
+          };
+          directionsService.route(request, function(response, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                  directionsDisplay.setDirections(response);
+                 }
+            });
+        }
+          
+       };
 
         $scope.getDateTime = function() {
             $scope.date = new Date();
