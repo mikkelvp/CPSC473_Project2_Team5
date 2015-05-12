36b9a214e7fc19f3680ce181f23fa60428e42e75
AndrewWesterman <awesterman1507@gmail.com>
2015-05-11 23:04:33 -0700

    Riders can now leave their rides

--- app/controllers/rideDetailsController.js
+++ app/controllers/rideDetailsController.js
@@ -22,9 +22,20 @@ rideshareControllers.controller('rideDetailsCtrl', ['$scope', '$rootScope', '$lo
             if(userid === ride.owner){
                 alert("This ride will be deleted");
             } else {
-                alert("You will be removed from this ride");
+                var updatedRiders = ride.riders;
+                for(var i = updatedRiders.length - 1; i >= 0; i--) {
+                    if(updatedRiders[i] === userid) {
+                       updatedRiders.splice(i, 1);
+                    }
+                }
+                $http.put('/api/ride/'+ride._id, {
+                    riders: updatedRiders
+                }).success(function(data, status, headers, config) {
+                    alert("You will be removed from this ride"); 
+                });
+                               
             }
             $location.path("myRides");
-        }
+        };
     }
 ]);
