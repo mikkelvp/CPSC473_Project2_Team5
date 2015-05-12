199f84565c0cd97fc61ea8fff3487be292eade34
AndrewWesterman <awesterman1507@gmail.com>
2015-05-11 21:43:24 -0700

    Users can no longer join full rides

--- app/controllers/searchResultsController.js
+++ app/controllers/searchResultsController.js
@@ -6,17 +6,22 @@ rideshareControllers.controller('searchResultsCtrl', ['$scope', '$http', '$rootS
         var query = JSON.parse(sessionStorage.query);
         $scope.searchResults = JSON.parse(sessionStorage.searchResults);
 
-        $scope.joinRide = function(id) {
-            console.log('id: ' + id);
-            $http.put('/api/ride/add', {
-                rideId: id,
-                personId: $scope.user._id
-            }).success(function(data, status, headers, config) {
-                console.log(data);
-                alert('Ride joined');
-                $rootScope.ride = data;
-                $location.path('/ride');
-            });
+        $scope.joinRide = function(ride) {
+            var id = ride._id;
+            if(ride.availableSeats - ride.riders.length > 0){
+                console.log('id: ' + id);
+                $http.put('/api/ride/add', {
+                    rideId: id,
+                    personId: $scope.user._id
+                }).success(function(data, status, headers, config) {
+                    console.log(data);
+                    alert('Ride joined');
+                    $rootScope.ride = data;
+                    $location.path('/ride');
+                });
+            } else {
+                alert("Ride is full");
+            }            
         };
 
 
